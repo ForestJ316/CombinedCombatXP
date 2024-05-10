d66f8e9554a7437960456a2eef796632c614d77d
ForestJ316 <91183260+ForestJ316@users.noreply.github.com>
2024-05-10 18:43:45 +0100

    Update Version 1.1

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -16,8 +16,8 @@ Settings::EventResult Settings::ProcessEvent(const RE::MenuModeChangeEvent& a_ev
 
 void Settings::ReadIniSettings()
 {
-	constexpr auto defaultini_path = L"Data/MCM/Config/Combined Combat XP Redux/settings.ini";
-	constexpr auto userini_path = L"Data/MCM/Settings/Combined Combat XP Redux.ini";
+	constexpr auto defaultini_path = L"Data/MCM/Config/CombinedCombatXP/settings.ini";
+	constexpr auto userini_path = L"Data/MCM/Settings/CombinedCombatXP.ini";
 
 	const auto ReadSettingsFile = [&](std::filesystem::path path) {
 		CSimpleIniA ini;
