9ac65de5208fe4c3e7f0f74962f51840b373bd97
ForestJ316 <91183260+ForestJ316@users.noreply.github.com>
2024-05-14 16:45:31 +0100

    Version 1.1.0

--- src/PCH.h
+++ src/PCH.h
@@ -1,26 +1,16 @@
 #pragma once
 
 #pragma warning(push)
-#include <RE/Skyrim.h>
+#include <F4SE/F4SE.h>
+#include <RE/Fallout.h>
 #include <REL/Relocation.h>
-#include <SKSE/SKSE.h>
 
-#ifdef NDEBUG
-#	include <spdlog/sinks/basic_file_sink.h>
-#else
-#	include <spdlog/sinks/msvc_sink.h>
-#endif
+#include <SimpleIni.h>
+
 #pragma warning(pop)
 
 using namespace std::literals;
 
-namespace logger = SKSE::log;
-
-namespace util
-{
-	using SKSE::stl::report_and_fail;
-}
-
 #define DLLEXPORT __declspec(dllexport)
 
-#include "Plugin.h"
+#include "Version.h"
--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,54 @@
+#include "Hooks.h"
+#include "Settings.h"
+
 namespace
 {
-	void InitializeLog()
+	void MessageHandler(F4SE::MessagingInterface::Message* a_msg)
 	{
-#ifndef NDEBUG
-		auto sink = std::make_shared<spdlog::sinks::msvc_sink_mt>();
-#else
-		auto path = logger::log_directory();
-		if (!path) {
-			util::report_and_fail("Failed to find standard logging directory"sv);
+		switch (a_msg->type)
+		{
+			case F4SE::MessagingInterface::kGameDataReady:
+				Settings::Install();
+				Settings::ReadIniSettings();
+				break;
+			case F4SE::MessagingInterface::kPreLoadGame:
+				Hooks::ResetVars();
+				break;
+			default:
+				break;
+		}
+	}
+
+	extern "C" DLLEXPORT bool F4SEAPI F4SEPlugin_Query(const F4SE::QueryInterface * a_f4se, F4SE::PluginInfo * a_info)
+	{
+		a_info->infoVersion = F4SE::PluginInfo::kVersion;
+		a_info->name = Version::PROJECT.data();
+		a_info->version = Version::MAJOR;
+
+		if (a_f4se->IsEditor()) {
+			return false;
 		}
 
-		*path /= fmt::format("{}.log"sv, Plugin::NAME);
-		auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
-#endif
+		const auto ver = a_f4se->RuntimeVersion();
+		if (ver < F4SE::RUNTIME_1_10_162) {
+			return false;
+		}
 
-#ifndef NDEBUG
-		const auto level = spdlog::level::trace;
-#else
-		const auto level = spdlog::level::info;
-#endif
-
-		auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
-		log->set_level(level);
-		log->flush_on(level);
-
-		spdlog::set_default_logger(std::move(log));
-		spdlog::set_pattern("%g(%#): [%^%l%$] %v"s);
+		return true;
 	}
-}
 
-extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() {
-	SKSE::PluginVersionData v;
+	extern "C" DLLEXPORT bool F4SEAPI F4SEPlugin_Load(const F4SE::LoadInterface * a_f4se)
+	{
+		F4SE::Init(a_f4se);
+		F4SE::AllocTrampoline(1 * 1024);
 
-	v.PluginVersion(Plugin::VERSION);
-	v.PluginName(Plugin::NAME);
+		if (!F4SE::GetMessagingInterface()->RegisterListener(MessageHandler))
+		{
+			return false;
+		}
 
-	v.UsesAddressLibrary(true);
-	v.CompatibleVersions({ SKSE::RUNTIME_LATEST });
+		Hooks::Hook();
 
-	return v;
-}();
-
-extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
-{
-	InitializeLog();
-	logger::info("{} v{}"sv, Plugin::NAME, Plugin::VERSION.string());
-
-	SKSE::Init(a_skse);
-
-	return true;
+		return true;
+	}
 }
